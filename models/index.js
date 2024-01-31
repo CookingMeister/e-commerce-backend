b8ad623024ff82c4000a657e7e6f83d35886dfe8
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-01-31 09:36:17 -0400

    add associations between Models in models/index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category // do
+Product.belongsTo(Category, { // done
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Categories have many Products  // do
+Category.hasMany(Product, {   // done
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag)  // do
+Product.belongsToMany(Tag, {
+  through: ProductTag,  //  done
+  foreignKey: 'product_id',
+  otherKey: 'tag_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag)  // do
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',   // done
+  otherKey: 'product_id'
+});
 
 module.exports = {
   Product,
