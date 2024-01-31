78bf0787e3d7082fbbf7f1a17645e512ec205737
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-01-31 18:15:39 -0400

    add Jest tests for categories, products, tags models, passed, working with --forceExit parameter only

--- __tests/test.js
+++ __tests/test.js
@@ -1,14 +1,15 @@
 const app = require('../server.js');
 const request = require('supertest');
-const { Category } = require('../models');
+const { Tag, Category } = require('../models');
+const sequelize = require('../config/connection.js');
 
 beforeAll(async () => {
-  await Category.sequelize.sync();
+  await sequelize.sync();
 });
 
 describe('GET /api/categories', () => {
   test('responds with all categories', async () => {
-    return request(app)
+    return await request(app)
       .get('/api/categories')
       .then((response) => {
         expect(response.statusCode).toBe(200);
@@ -17,10 +18,10 @@ describe('GET /api/categories', () => {
       });
   });
   test('includes associated products', async () => {
-    const response = await request(app)
+    return await request(app)
       .get('/api/categories')
       .then((response) => {
-        expect(response.body[0].products).toBeDefined(); // good
+        expect(response.body[0].products).toBeDefined();
         expect(Array.isArray(response.body[0].products)).toBeTruthy();
       });
   });
@@ -29,15 +30,18 @@ describe('GET /api/categories', () => {
 describe('GET /api/categories/:id', () => {
   test('responds with a category by id', async () => {
     const category = await Category.findByPk(1);
-    await request(app)
+    return await request(app)
       .get(`/api/categories/${category.id}`)
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.body.id).toBe(category.id);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   });
   test('includes associated products', async () => {
-    const response = await request(app)
+    return await request(app)
       .get(`/api/categories/${1}`) // good
       .then((response) => {
         expect(response.body.products).toBeDefined();
@@ -45,9 +49,54 @@ describe('GET /api/categories/:id', () => {
       });
   });
 });
-
-afterAll(async () => {
-  await Category.sequelize.close();
+describe('GET /api/products', () => {
+  test('responds with all products', async () => {
+    return await request(app)
+      .get('/api/products')
+      .expect(200)
+      .then((response) => {
+        expect(Array.isArray(response.body)).toBeTruthy();
+      });
+  });
 });
 
+describe('GET /api/products/:id', () => {
+  test('responds with a product by id', async () => {
+    const productId = 1;
+    return await request(app)
+      .get(`/api/products/${productId}`)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.id).toBe(productId);
+      });
+  });
+});
+describe('GET /api/tags', () => {
+  it('should return all tags with associated products', async () => {
+    const res = await request(app).get('/api/tags');
 
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0].products).toBeDefined();
+  });
+});
+
+describe('GET /api/tags/:id', () => {
+  it('should return tag by id with associated products', async () => {
+    const tag = await Tag.findOne({
+      where: {
+        id: 1,
+      },
+    });
+
+    const res = await request(app).get(`/api/tags/${tag.id}`);
+
+    expect(res.status).toBe(200);
+    expect(res.body.id).toBe(tag.id);
+    expect(res.body.products).toBeDefined();
+  });
+});
+
+afterAll(async () => {
+  await sequelize.close();
+});
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     .then((dbTagData) => res.json(dbTagData))
     .catch((err) => {
       console.log(err);
-      res.status(500).json(err);
+      res.status(404).json(err);
     });
 });
 router.get('/:id', (req, res) => {
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
     .then((dbTagData) => res.json(dbTagData))
     .catch((err) => {
       console.log(err);
-      res.status(500).json(err);
+      res.status(404).json(err);
     });
 });
 
